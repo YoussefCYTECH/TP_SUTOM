c45f11a8538f34359d272b03f650f4a115621f1d
Youssef <agoussalyo@cy-tech.fr>
2022-09-15 15:09:02 +0200

    Setup structure fichiers

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const http = require('http');
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(express.static("www"))
+app.use('/', express.static('static'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
