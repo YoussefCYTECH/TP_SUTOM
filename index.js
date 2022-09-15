314ec9a40ae307920b58194ebad8876979588d72
Youssef <agoussalyo@cy-tech.fr>
2022-09-15 17:13:03 +0200

    Passage du mot à l'html

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+
+// Importation des mots :
+
+var fs = require('fs');
+var text_array = fs.readFileSync('data/liste_francais_utf8.txt').toString().split("\n");
+
+
+// Choix du mot du jour :
+
+/*function getRandomInt(max) { //Changer en génération par date
+    return Math.floor(Math.random() * max);
+}
+console.log(text_array.find(getRandomInt(text_array.length)));
+var mot = text_array.find(getRandomInt(text_array.length));*/
+
+var mot = text_array[1];
+console.log(mot);
+
+//global.mot = mot;
+
+
+
+
+app.get('/mot', (req, res) => {
+    res.send(mot);
+    //res.render('index.html', {mot:mot});
 })
 
 app.use(express.static("www"))
--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,16 @@
-alert('OK')
+
+// Convert text data to array
+
+const fs = require('fs');
+
+fs.readFile('data/liste_francais_utf8.txt', (err, data) => {
+    if (err) throw err;
+    array_data = data.toString().split('\r\n');
+    console.log(array_data);
+});
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+console.log(textArray.find(getRandomInt(textArray.length)));
