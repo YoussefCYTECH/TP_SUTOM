961d451bc80a081e68949f943594fb5866f71518
Youssef <agoussalyo@cy-tech.fr>
2022-09-29 14:24:19 +0200

    TP Score fonctionnel
    Mais améliorer les visuels, et le fixe pour l'instant

---
+++ README.md
@@ -0,0 +1,26 @@
+# SUTOM, a motus app
+
+## Description 
+
+Motus app running with micro services
+
+ ## Usage 
+ 
+ use with standard node command
+ 
+ ```
+ npm install
+ node index.js
+ ```
+
+go to http://localhost:3000
+
+
+# Author
+Youssef AGOUSSAL ING3 IA1
+Cy Tech Cergy
+
+
+# TODO
+- Mot aléatoire chaque jour et non fixe
+- visuels améliorés
--- index.js
+++ index.js
@@ -2,38 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
-
-// Importation des mots :
-
-var fs = require('fs');
-var text_array = fs.readFileSync('data/liste_francais_utf8.txt').toString().split("\n");
-
-
-// Choix du mot du jour :
-
-/*function getRandomInt(max) { //Changer en génération par date
-    return Math.floor(Math.random() * max);
-}
-console.log(text_array.find(getRandomInt(text_array.length)));
-var mot = text_array.find(getRandomInt(text_array.length));*/
-
-var mot = text_array[1];
-console.log(mot);
-
-//global.mot = mot;
-
-
-
-
-app.get('/mot', (req, res) => {
-    res.send(mot);
-    //res.render('index.html', {mot:mot});
-})
-
 app.use(express.static("www"))
 app.use('/', express.static('static'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+
+// Fonctions de redirections :
+
+app.get('/mot', (req, res) => {
+
+    var fs = require('fs');
+    var text_array = fs.readFileSync('data/liste_francais_utf8.txt').toString().split("\n");
+    var mot = text_array[1];
+    mot = "vendredi"; //Pour test
+
+/*
+    const d = new Date();
+    let day = d.getDay();
+    let word = ["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"][day]*/
+
+    res.send(mot);
 })
